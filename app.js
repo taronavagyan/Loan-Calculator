f4cd02a06ebabce11dba7dfb6cee269269892ace
Taron Avagyan <contact@taronavagyan.com>
2022-12-10 16:45:27 -0800

    html

--- app.js
+++ app.js
@@ -5,6 +5,53 @@ const URL = require("url").URL;
 const PORT = 3000;
 const APR = 5;
 
+const HTML_START = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Loan Calculator</title>
+    <style type="text/css">
+    body {
+      background: rgba(250, 250, 250);
+      font-family: sans-serif;
+      color: rgb(50, 50, 50);
+    }
+
+    article {
+      width: 100%;
+      max-width: 40rem;
+      margin: 0 auto;
+      padding: 1rem 2rem;
+    }
+
+    h1 {
+      font-size: 2.5rem;
+      text-align: center;
+    }
+
+    table {
+      font-size: 2rem;
+    }
+
+    th {
+      text-align: right;
+    }
+  </style>
+  </head>
+  <body>
+    <article>
+      <h1>Loan Calculator</h1>
+      <table>
+        <tbody>`;
+
+const HTML_END = `
+        </tbody>
+      </table>
+    </article>
+  </body>
+</html>`;
+
 function getParams(path) {
   const myURL = new URL(path, `http://localhost:${PORT}`);
   return myURL.searchParams;
@@ -18,8 +65,10 @@ function getLoanInfo(params) {
     APR,
     durationInYears
   );
-  let body = `Amount: $${amountInDollars}\nDuration: ${durationInYears} years\nAPR: ${APR}%\nMonthly Payment: $${monthlyPayments}`;
-  return body;
+  return `<tr><th>Amount:</th><td>$${amountInDollars}</td></tr>
+  <tr><th>Duration:</th><td>${durationInYears} years</td></tr>
+  <tr><th>APR:</th><td>${APR}%</td></tr>
+  <tr><th>Monthly payment:</th><td>$${monthlyPayments}</td></tr>`;
 }
 
 function calculateMonthlyPayments(amountInDollars, APR, durationInYears) {
@@ -34,15 +83,16 @@ function calculateMonthlyPayments(amountInDollars, APR, durationInYears) {
 const SERVER = HTTP.createServer((req, res) => {
   let path = req.url;
 
-  let loanInfo = getLoanInfo(getParams(path));
+  let HTML_MIDDLE = getLoanInfo(getParams(path));
+  let content = HTML_START + HTML_MIDDLE + HTML_END;
 
   if (path === "/favicon.ico") {
     res.statusCode = 404;
     res.end();
   } else {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.write(loanInfo);
+    res.setHeader("Content-Type", "text/html");
+    res.write(content);
     res.end();
   }
 });
